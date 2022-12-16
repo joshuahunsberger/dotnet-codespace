9896b836c438a72f0801a5eb25dc0afa8f4d8a88
Joshua Hunsberger <joshua@hunsberger.co>
2022-12-16 20:57:56 +0000

    Add console project to make requests to API

---
+++ console/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Json;
+
+using var client = new HttpClient();
+var response = await client.GetFromJsonAsync<WeatherApi.WeatherForecast[]>("https://localhost:7077/WeatherForecast");
+
+if (response == null) {
+    Console.WriteLine("No response");
+    return;
+}
+
+foreach (var item in response)
+{
+    Console.WriteLine($"{item.Date} - {item.Summary} - {item.TemperatureF} F / {item.TemperatureC} C");
+}
